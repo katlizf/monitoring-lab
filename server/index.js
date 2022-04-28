2ca9bb8e1e547cb3b26cc6a77e745eb7fcc165dd
Katie Anderson <katherine.lizf@gmail.com>
2022-04-28 17:42:08 -0500

    back to just html

--- server/index.js
+++ server/index.js
@@ -7,17 +7,16 @@ const app = express()
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../styles/index.html'))
+    res.sendFile(path.join(__dirname, '../index.html'))
     rollbar.info('html file served successfully.')
 })
 
-app.get('/style', (req, res) => {
-    res.sendFile(path.join(__dirname, '../styles/index.css'))
-    rollbar.info('css file served')
-})
+// app.get('/style', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../styles/index.css'))
+//     rollbar.info('css file served')
+// })
 
-// app.use(express.static("styles"))
-app.use(express.static(path.join(__dirname, '../styles/index.css')))
+// app.use(express.static(path.join(__dirname, '../styles/index.css')))
 
 let rollbar = new Rollbar({
     accessToken: '3ab6e5a8142347e894de5e4e9bef7fc7',
