819e87d479260f4a8780c6b1672fc80717dd6162
Katie Anderson <katherine.lizf@gmail.com>
2022-04-28 16:54:21 -0500

    push files

--- server/index.js
+++ server/index.js
@@ -1,18 +1,22 @@
 const express = require('express')
 const path = require('path')
-
+const Rollbar = require("rollbar")
 
 const app = express()
 app.use(express.json())
 
-
-
-
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
-    // rollbar.info('html file served successfully.')
+    rollbar.info('html file served successfully.')
 })
 
+let rollbar = new Rollbar({
+    accessToken: '3ab6e5a8142347e894de5e4e9bef7fc7',
+    captureUncaught: true,
+    captureUnhandledRejections: true
+})
+
+rollbar.log("Hello again!")
 
 const port = process.env.PORT || 4554
 
