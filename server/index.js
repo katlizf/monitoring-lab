9a18e42fa613df1cc6f259a215e3c9994a99face
Katie Anderson <katherine.lizf@gmail.com>
2022-04-28 17:33:15 -0500

    please

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../index.html'))
+    res.sendFile(path.join(__dirname, '../styles/index.html'))
     rollbar.info('html file served successfully.')
 })
 
